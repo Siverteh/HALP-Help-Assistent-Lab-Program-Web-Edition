d26ac21f51f0b1f5e72b3b282132d63c4bb7afa6
Sondre Horpestad <sondreho@uia.no>
2022-10-31 12:32:18 +0100

    CHAN-6 Made the model for courses to the db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         : base(options)
     {
     }
+    
+    public DbSet<>
 }
---
+++ Models/CourseModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Framework;
+
+namespace OperationCHAN.Models;
+
+public class CourseModel
+{
+    public CourseModel(){}
+
+    public CourseModel(string courseCode, string courseRooms, DateTime couseStart, DateTime courseEnd)
+    {
+        CourseCode = courseCode;
+        CourseStart = couseStart;
+        CourseEnd = courseEnd;
+        CourseRoom1 = courseRooms;
+    }
+    
+    public int Id { get; set; }
+    
+    [Required]
+    public string CourseCode { get; set; } = String.Empty;
+    
+    [Required]
+    public string CourseRoom1 { get; set; } = String.Empty;
+    public string CourseRoom2 { get; set; } = String.Empty;
+    public string CourseRoom3 { get; set; } = String.Empty;
+    public string CourseRoom4 { get; set; } = String.Empty;
+    
+    
+    [Required]
+    public DateTime CourseStart { get; set; }
+    
+    [Required]
+    public DateTime CourseEnd { get; set; }
+    
+    public int Age { get; set; }
+    
+    
+}
