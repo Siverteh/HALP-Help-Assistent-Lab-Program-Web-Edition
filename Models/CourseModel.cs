d5698140bd49ed3933c158dd693536d94a8f6eb3
Nikolai Bunch Eidsheim <nikolaie@uia.no>
2022-11-01 11:47:33 +0100

    CHAN-6 Code cleanup

--- Models/CourseModel.cs
+++ Models/CourseModel.cs
@@ -6,36 +6,16 @@ public class CourseModel
 {
     public CourseModel(){}
 
-    public CourseModel(string courseCode, DateTime courseEnd, DateTime courseStart, string courseRooms)
+    public CourseModel(string courseCode, DateTime courseEnd, DateTime courseStart, string[] courseRooms)
     {
-        var code = courseCode.Split(',');
-        CourseCode = code[0];
-        
+        CourseCode = courseCode;
         CourseStart = courseStart;
         CourseEnd = courseEnd;
 
-        var courseRoom = courseRooms.Split(',', '/');
-
-        for (int i = 0; i < courseRoom.Length; i++)
-        {
-            if (courseRoom[i].Contains("GRM"))
-            {
-                if (courseRoom[i][0] == ' ')
-                {
-                    courseRoom[i] = courseRoom[i].Remove(0, 1);
-                }
-            }
-            else
-            {
-                var tmp = courseRoom[i-1].Substring(0, 7);
-                courseRoom[i] = tmp + courseRoom[i];
-            }
-        }
-        
-        CourseRoom1 = courseRoom[0];
-        if (courseRoom.Length > 1) CourseRoom2 = courseRoom[1];
-        if (courseRoom.Length > 2) CourseRoom3 = courseRoom[2];
-        if (courseRoom.Length > 3) CourseRoom4 = courseRoom[3];
+        CourseRoom1 = courseRooms[0];
+        if (courseRooms.Length > 1) CourseRoom2 = courseRooms[1];
+        if (courseRooms.Length > 2) CourseRoom3 = courseRooms[2];
+        if (courseRooms.Length > 3) CourseRoom4 = courseRooms[3];
         
     }
     
