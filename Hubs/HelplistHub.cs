cfee29a6a14784bc96e8e5729e0a24d377eec643
Nikolai Bunch Eidsheim <nikolaie@uia.no>
2022-11-10 09:38:10 +0100

    CHAN-51 Started work on group support

--- Hubs/HelplistHub.cs
+++ Hubs/HelplistHub.cs
@@ -14,5 +14,39 @@ namespace OperationCHAN.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", nickname, description, room);
         }
+        
+        /// <summary>
+        /// Send a message to a specific group
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="nickname"></param>
+        /// <param name="description"></param>
+        /// <param name="room"></param>
+        public async Task SendMessageToGroup(string user, string nickname, string description, string room)
+        {
+            await Clients.User(user).SendAsync("ReceiveMessage", nickname, description, room);
+        }
+        
+        /// <summary>
+        /// Add the user to the group
+        /// </summary>
+        /// <param name="groupName"></param>
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            //await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        /// <summary>
+        /// Remove a user from the group
+        /// </summary>
+        /// <param name="groupName"></param>
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            //await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
     }
 }
