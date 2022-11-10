a81c2492b914311d71e9a589a430522062979499
Nikolai Bunch Eidsheim <nikolaie@uia.no>
2022-11-10 13:43:26 +0100

    CHAN-52 Continued work on archiving

--- Hubs/HelplistHub.cs
+++ Hubs/HelplistHub.cs
@@ -16,6 +16,17 @@ namespace OperationCHAN.Hubs
         }
         
         /// <summary>
+        /// Sends a message to all connected clients
+        /// </summary>
+        /// <param name="nickname">The nickname to show</param>
+        /// <param name="description">The description to show</param>
+        /// <param name="room">The room you are in</param>
+        public async Task UnArchive(string nickname, string description, string room)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", nickname, description, room);
+        }
+        
+        /// <summary>
         /// Send a message to a specific group
         /// </summary>
         /// <param name="user"></param>
