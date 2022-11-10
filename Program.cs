980688583398034187652f90944f4aa9162f3158
Nikolai Bunch Eidsheim <nikolaie@uia.no>
2022-11-10 11:22:00 +0100

    CHAN-52 Added archive and nav buttons

--- Program.cs
+++ Program.cs
@@ -81,18 +81,11 @@ app.MapRazorPages();
 
 app.MapHub<HelplistHub>("/chatHub");
 
-// Route added for debugging purposes, to see all available endpoints
-app.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
-    string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
-
 // Start TimeEdit loop
 new Timeedit(app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>()).StartLoop();
 
 // Route added for debugging purposes, to see all available endpoints
 app.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
     string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
-
-// Start TimeEdit loop
-new Timeedit(app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>()).StartLoop();
 
 app.Run();
