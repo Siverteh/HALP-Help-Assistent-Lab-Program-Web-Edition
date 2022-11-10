980aac47f8ed82a903158857550f4e6125d55f95
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2022-11-10 12:10:52 +0100

    CHAN-9: moved out form to razor file

---
+++ Areas/Ticket/Pages/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OperationCHAN.Data;
+using OperationCHAN.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OperationCHAN.Areas.Ticket.Pages;
+
+public class Create : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Create(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+    
+    [BindProperty]
+    public HelplistModel Ticket { get; set; } = default!;
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        Console.WriteLine(Ticket);
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        var date = new DateTime().ToString("dd/MM/yyyy hh:mm");
+        Ticket.DateTime = Convert.ToDateTime(date);
+        Ticket.Status = "Waiting";
+        
+        _db.HelpList.Add(Ticket);
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("./Create");
+    }
+
+}
--- Models/HelplistModel.cs
+++ Models/HelplistModel.cs
@@ -1,19 +1,40 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OperationCHAN.Models;
 
 public class HelplistModel
 {
     public HelplistModel(){}
-
-    public int Id { get; set; }
-
-    public string Nickname { get; set; } = String.Empty;
     
-    public string Room { get; set; } = String.Empty;
-    
+    public HelplistModel(string nickname, string description, string room, DateTime dateTime)
+    {
+        Nickname = nickname;
+        Description = description;
+        Room = room;
+        DateTime = dateTime;
+    }
+
+
     public string Status { get; set; } = String.Empty;
     
-    public string Description { get; set; } = String.Empty;
+    public int Id { get; set; }
+
+    [Required]
+    [DataType(DataType.Text)]
+    public string Nickname { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Text)]
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Text)]
+    public string Room { get; set; } = string.Empty;
+
+    
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+    public DateTime DateTime { get; set; }
 
 }
