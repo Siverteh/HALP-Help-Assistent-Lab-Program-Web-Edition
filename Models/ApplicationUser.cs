19b623fea075ac729765d22dacbeadd2eae68d1b
Ole-Johan Ovreas <olejohanjo@student.uia.no>
2022-12-13 12:17:13 +0100

    CHAN-80: Added warning messages if user registered with Discord tries to log in normally and if user tries to log in before email is confirmed

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -6,16 +6,19 @@ public class ApplicationUser : IdentityUser
 {
     public ApplicationUser() {}
 
-    public ApplicationUser(string nickname, string discordTag)
+    public ApplicationUser(string nickname, string discordTag, bool discordLogin)
     {
         Nickname = nickname;
         DiscordTag = discordTag;
+        DiscordLogin = discordLogin;
     }
     
     //public int Id { get; set; }
     public string DiscordTag { get; set; } = String.Empty;
 
     public string Nickname{ get; set; } = String.Empty;
+    
+    public bool DiscordLogin { get; set; } = false;
     
     public List<HelplistModel> Helplist { get; set; } = new List<HelplistModel>();
 
